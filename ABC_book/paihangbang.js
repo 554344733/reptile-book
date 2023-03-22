cbc9cb31d5c71d86a99f4326c9bb1bb58666170e
DY <554344733@qq.com>
2023-03-22 18:22:26 +0800

    feat: 描述文件

--- ABC_book/paihangbang.js
+++ ABC_book/paihangbang.js
@@ -1,25 +1,25 @@
 const cheerio = require('cheerio');
-// const axios = require('axios');
 const { request, BASE_URL } = require('../request');
-
 const { sql_addData, sql_search } = require('../data_utils');
-const { connection } = require('../data_bank');
-const biaoming = 'book_dushi';
-const bookType = '都市言情';
-const allPage = 200;
-const bookPage = 114;
+
+// 方法中参数抽离
+const sheetName = 'book_dushi'; // 数据库的表名
+const bookType = '都市言情'; //当前book要存储什么类型，因为首页没有小说类型，进入详情一个个存储的话太慢
+const targetPage = 200; // 要存储到多少页
+const bookTypeId = 114; // 小说类型对应的id
+
 // 获取ABC排行榜数据
 async function getABCRanking(page) {
 	try {
-		if (page >= allPage) {
+		if (page >= targetPage) {
 			return;
 		}
-		const html = await request(`/paihangbang/all${bookPage}_${page}.html`);
+		const html = await request(`/paihangbang/all${bookTypeId}_${page}.html`);
 		const $ = cheerio.load(html);
 		$('#main .hot_sale').map(async (i, item) => {
-			// 先拿到小说id做个判断，如果存在就不往数据库存
+			// 先拿到当前小说id做个判断，如果存在就不往数据库存
 			let bookId = $(item).children('a').attr('href').split('/')[2];
-			const isBook = await sql_search(biaoming, bookId);
+			const isBook = await sql_search(sheetName, bookId);
 			if (isBook.length) {
 				console.log('当前id为' + bookId + '的书籍数据库已经存在');
 				if (i === $('#main .hot_sale').length - 1) {
@@ -37,7 +37,7 @@ async function getABCRanking(page) {
 					bookType: bookType,
 					currentPage: page,
 				};
-				await sql_addData(biaoming, bookObj);
+				await sql_addData(sheetName, bookObj);
 				console.log('小说:' + bookObj.bookName + '入库成功！');
 
 				if (i === $('#main .hot_sale').length - 1) {
---
+++ README.md
@@ -0,0 +1,12 @@
+1、下载依赖
+npm intall
+
+2、需要 data_bank 配置自己的数据库信息
+
+3、根据自己的情况更改 ABC_book/paihangbang.js 的代码
+
+![image-20230322181946265](/Users/daiyang/Desktop/myProject/reptile-book/assets/image-20230322181946265.png)
+
+4、数据库
+
+![image-20230322182125474](/Users/daiyang/Desktop/myProject/reptile-book/assets/image-20230322182125474.png)
